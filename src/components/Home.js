22c8c48aba3d4fd1432cc8c403e8307ad3147de0
AtharvaCM <necrons999@gmail.com>
2022-08-26 14:30:08 +0530

    axios and helper function

---
+++ src/components/Home.js
@@ -0,0 +1,26 @@
+// Example for API calling
+
+import React, { useMemo } from "react";
+
+// custom hooks
+import { useAxios } from "../hooks/useAxios";
+
+const url = "https://reqres.in/api/users/2";
+const method = "GET";
+const payload = {};
+
+const Home = () => {
+  const { data, error, loaded } = useAxios(url, method, payload);
+
+  const stringifiedData = useMemo(() => {
+    return JSON.stringify(data || {});
+  }, [data]);
+
+  if (loaded) {
+    return error ? <p>Something went wrong</p> : <p>{stringifiedData}</p>;
+  }
+
+  return <span>Loading...</span>;
+};
+
+export default Home;
--- src/router/Router.js
+++ src/router/Router.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 import { Routes, Route } from "react-router-dom";
+import Home from "../components/Home";
 
 const Router = () => {
   return (
     <Routes>
-      <Route index path="/" element={<p>Home Page</p>}></Route>
+      <Route index path="/" element={<Home />}></Route>
     </Routes>
   );
 };
 
 export default Router;
+
