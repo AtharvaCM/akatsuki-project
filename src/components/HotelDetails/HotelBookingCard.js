1037d409a344688095ced0d73b1c425d22712004
SahilSK202 <sahil.kavitake202@gmail.com>
2022-09-05 23:30:36 +0530

    Added Date and Price Validations

--- src/components/HotelDetails/HotelBookingCard.js
+++ src/components/HotelDetails/HotelBookingCard.js
@@ -24,24 +24,26 @@ import dayjs from "dayjs";
 // CSS
 import styles from "./HotelBookingCard.module.css";
 
-const roomPrice = 720;
-const extraFeatures = [
-  {
-    feature: "Allow to bring pet",
-    price: 15,
-  },
-  { feature: "Lunch per person per day", price: 24 },
-  { feature: "Parking", price: 5 },
-  { feature: "Extra Pillow", price: 0 },
-];
-
 const HotelBookingCard = () => {
+  // Dummy data
+  const roomPrice = 720;
+  const extraFeatures = [
+    {
+      feature: "Allow to bring pet",
+      price: 15,
+    },
+    { feature: "Lunch per person per day", price: 24 },
+    { feature: "Parking", price: 5 },
+    { feature: "Extra Pillow", price: 0 },
+  ];
   // React Hooks
   const [checkInDate, setCheckInDate] = useState(null);
   const [checkOutDate, setCheckOutDate] = useState(null);
   const [guestsCount, setGuestsCount] = useState(1);
   const [roomsCount, setRoomsCount] = useState(1);
-  const [totalAmount, setTotalAmount] = useState(roomPrice);
+  const [extraFeatureAmount, setExtraFeatureAmount] = useState(0);
+  const [numberOfDays, setNumberOfDays] = useState(1);
+  const [totalAmount, setTotalAmount] = useState(roomPrice * numberOfDays);
 
   const decrementGuestCount = () => {
     if (guestsCount > 1) {
@@ -56,21 +58,68 @@ const HotelBookingCard = () => {
   const decrementRoomsCount = () => {
     if (roomsCount > 1) {
       setRoomsCount((prevState) => +prevState - 1);
-      setTotalAmount((prevState) => prevState - roomPrice);
+      setTotalAmount((prevState) => prevState - roomPrice * numberOfDays);
     }
   };
 
   const incrementRoomsCount = () => {
     setRoomsCount((prevState) => +prevState + 1);
-    setTotalAmount((prevState) => prevState + roomPrice);
+    setTotalAmount((prevState) => prevState + roomPrice * numberOfDays);
+  };
+
+  const guestsInputOnChangeHandler = (e) => {
+    setGuestsCount(
+      e.target.value.length === 0 || e.target.value < 1 ? "" : e.target.value
+    );
+  };
+
+  const roomInputOnChangeHandler = (e) => {
+    let newRoomsCount =
+      e.target.value.length === 0 || e.target.value < 1 ? "" : e.target.value;
+
+    setRoomsCount(newRoomsCount);
+    setTotalAmount(
+      extraFeatureAmount + newRoomsCount * +roomPrice * numberOfDays
+    );
   };
 
   const ExtraFeaturesChangeHandler = (e) => {
     if (e.target.checked) {
-      setTotalAmount((prevState) => prevState + +e.target.name);
+      setExtraFeatureAmount(
+        (prevState) => prevState + +e.target.name * numberOfDays
+      );
+      setTotalAmount((prevState) => prevState + +e.target.name * numberOfDays);
     } else {
-      setTotalAmount((prevState) => prevState - +e.target.name);
+      setExtraFeatureAmount(
+        (prevState) => prevState - +e.target.name * numberOfDays
+      );
+      setTotalAmount((prevState) => prevState - +e.target.name * numberOfDays);
     }
+  };
+
+  const datesChangeHandler = (isCheckIn, newDate) => {
+    if (isCheckIn) {
+      setCheckInDate(newDate);
+      // If Check-In date crosses check-Out date
+      // Then set Check-Out date tommorow of Check-in Date
+      var days = dayjs(newDate).diff(dayjs(checkOutDate), "day");
+      if (days >= 0) {
+        setCheckOutDate(dayjs(newDate).add(1, "day"));
+      }
+      days = days >= 0 || !isNaN(days) ? Math.abs(days) : 1;
+      setNumberOfDays(days);
+    } else {
+      days = dayjs(newDate).diff(dayjs(checkInDate), "day");
+      setCheckOutDate(newDate);
+      setNumberOfDays(days);
+    }
+    setTotalAmount(
+      roomPrice * (isNaN(days) ? 1 : days) * roomsCount + extraFeatureAmount
+    );
+  };
+
+  const submitHandler = (e) => {
+    e.preventDefault();
   };
 
   return (
@@ -108,168 +157,163 @@ const HotelBookingCard = () => {
           </Grid>
         </Box>
         <Divider />
-        <Box className={styles["dates_container"]} sx={{ flexGrow: 1 }}>
-          <Grid container className={styles["card_header"]}>
-            <Grid item xs={5} md={5}>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  clearable={true}
-                  label="Check in"
-                  value={checkInDate}
-                  minDate={dayjs().add(1, "day")}
-                  inputFormat="DD/MM/YYYY"
-                  placeholder="DD/MM/YYYY"
-                  className={styles["input"]}
-                  onChange={(newCheckInDate) => {
-                    setCheckInDate(newCheckInDate);
-                    // If Check-In date crosses check-Out date
-                    // Then set Check-Out date tommorow of Check-in Date
-                    if (
-                      dayjs(newCheckInDate).diff(dayjs(checkOutDate), "day") >=
-                      0
-                    ) {
-                      setCheckOutDate(dayjs(newCheckInDate).add(1, "day"));
-                    }
-                  }}
-                  renderInput={(params) => (
-                    <TextField {...params} required={true} />
-                  )}
+        <form onSubmit={submitHandler}>
+          <Box className={styles["dates_container"]} sx={{ flexGrow: 1 }}>
+            <Grid container className={styles["card_header"]}>
+              <Grid item xs={5} md={5}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    clearable={true}
+                    label="Check in"
+                    value={checkInDate}
+                    minDate={dayjs().add(1, "day")}
+                    inputFormat="DD/MM/YYYY"
+                    placeholder="DD/MM/YYYY"
+                    className={styles["input"]}
+                    onChange={(newCheckInDate) => {
+                      datesChangeHandler(true, newCheckInDate);
+                    }}
+                    renderInput={(params) => (
+                      <TextField {...params} required={true} />
+                    )}
+                  />
+                </LocalizationProvider>
+              </Grid>
+              <Grid item xs={2} md={2}></Grid>
+              <Grid item xs={5} md={5}>
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DatePicker
+                    label="Check Out"
+                    disabled={!checkInDate && true}
+                    minDate={checkInDate && dayjs(checkInDate).add(1, "day")}
+                    value={checkOutDate}
+                    inputFormat="DD/MM/YYYY"
+                    placeholder="DD/MM/YYYY"
+                    className={styles["input"]}
+                    onChange={(newCheckOutDate) => {
+                      datesChangeHandler(false, newCheckOutDate);
+                    }}
+                    renderInput={(params) => (
+                      <TextField {...params} required={true} />
+                    )}
+                  />
+                </LocalizationProvider>
+              </Grid>
+            </Grid>
+          </Box>
+
+          <Box>
+            <Grid container className={styles["card_header"]}>
+              <Grid item xs={2}>
+                <IconButton onClick={decrementGuestCount}>
+                  <IndeterminateCheckBoxIcon
+                    fontSize="large"
+                    color="info"
+                  ></IndeterminateCheckBoxIcon>
+                </IconButton>
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  sx={{ marginTop: "4%", width: "100%" }}
+                  id="outlined-number"
+                  label="Number of Guests"
+                  type="number"
+                  required
+                  value={guestsCount}
+                  onChange={guestsInputOnChangeHandler}
                 />
-              </LocalizationProvider>
+              </Grid>
+              <Grid item xs={2}>
+                <IconButton onClick={incrementGuestCount}>
+                  <AddBoxIcon fontSize="large" color="info"></AddBoxIcon>
+                </IconButton>
+              </Grid>
             </Grid>
-            <Grid item xs={2} md={2}></Grid>
-            <Grid item xs={5} md={5}>
-              <LocalizationProvider dateAdapter={AdapterDayjs}>
-                <DatePicker
-                  label="Check Out"
-                  disabled={!checkInDate && true}
-                  minDate={checkInDate && dayjs(checkInDate).add(1, "day")}
-                  value={checkOutDate}
-                  inputFormat="DD/MM/YYYY"
-                  placeholder="DD/MM/YYYY"
-                  className={styles["input"]}
-                  onChange={(newCheckOutDate) => {
-                    setCheckOutDate(newCheckOutDate);
-                  }}
-                  renderInput={(params) => (
-                    <TextField {...params} required={true} />
-                  )}
+
+            <Grid container className={styles["card_header"]}>
+              <Grid item xs={2}>
+                <IconButton
+                  onClick={decrementRoomsCount}
+                  disabled={checkOutDate === null}
+                >
+                  <IndeterminateCheckBoxIcon
+                    fontSize="large"
+                    color="info"
+                  ></IndeterminateCheckBoxIcon>
+                </IconButton>
+              </Grid>
+              <Grid item xs={8}>
+                <TextField
+                  sx={{ marginTop: "4%", width: "100%" }}
+                  id="outlined-number"
+                  label="Number of Rooms"
+                  type="number"
+                  value={roomsCount}
+                  disabled={checkOutDate === null}
+                  required
+                  onChange={roomInputOnChangeHandler}
                 />
-              </LocalizationProvider>
-            </Grid>
-          </Grid>
-        </Box>
-
-        <Box>
-          <Grid container className={styles["card_header"]}>
-            <Grid item xs={2}>
-              <IconButton onClick={decrementGuestCount}>
-                <IndeterminateCheckBoxIcon
-                  fontSize="large"
-                  color="info"
-                ></IndeterminateCheckBoxIcon>
-              </IconButton>
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                sx={{ marginTop: "4%", width: "100%" }}
-                id="outlined-number"
-                label="Number of Guests"
-                type="number"
-                InputProps={{
-                  inputProps: { min: 1 },
-                }}
-                value={guestsCount}
-                onChange={(e) =>
-                  setGuestsCount(e.target.value < 1 ? 1 : e.target.value)
-                }
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <IconButton onClick={incrementGuestCount}>
-                <AddBoxIcon fontSize="large" color="info"></AddBoxIcon>
-              </IconButton>
-            </Grid>
-          </Grid>
-
-          <Grid container className={styles["card_header"]}>
-            <Grid item xs={2}>
-              <IconButton onClick={decrementRoomsCount}>
-                <IndeterminateCheckBoxIcon
-                  fontSize="large"
-                  color="info"
-                ></IndeterminateCheckBoxIcon>
-              </IconButton>
-            </Grid>
-            <Grid item xs={8}>
-              <TextField
-                sx={{ marginTop: "4%", width: "100%" }}
-                id="outlined-number"
-                label="Number of Rooms"
-                type="number"
-                InputProps={{
-                  inputProps: { min: 1 },
-                }}
-                value={roomsCount}
-                onChange={(e) => {
-                  setRoomsCount(e.target.value < 1 ? 1 : e.target.value);
-                  setTotalAmount(roomPrice + e.target.value * +roomPrice);
-                }}
-              />
-            </Grid>
-            <Grid item xs={2}>
-              <IconButton onClick={incrementRoomsCount}>
-                <AddBoxIcon fontSize="large" color="info"></AddBoxIcon>
-              </IconButton>
-            </Grid>
-          </Grid>
-        </Box>
-
-        <Typography className={styles["night_text"]}>Extra Features</Typography>
-        <Box>
-          {extraFeatures.map((extraFeature) => (
-            <Grid
-              key={extraFeature.feature}
-              container
-              className={styles["card_header"]}
-            >
-              <Grid item xs={2} md={2}>
-                <Checkbox
-                  name={extraFeature.price.toString()}
-                  onChange={ExtraFeaturesChangeHandler}
-                ></Checkbox>
               </Grid>
-              <Grid item xs={8} md={8}>
-                {extraFeature.feature}
-              </Grid>
-              <Grid className={styles["night_text"]} item xs={2} md={2}>
-                ${extraFeature.price}
+              <Grid item xs={2}>
+                <IconButton
+                  onClick={incrementRoomsCount}
+                  disabled={checkOutDate === null}
+                >
+                  <AddBoxIcon fontSize="large" color="info"></AddBoxIcon>
+                </IconButton>
               </Grid>
             </Grid>
-          ))}
-        </Box>
-        <Grid container>
-          <Grid item xs={9}>
-            <Typography className={styles["night_text"]}>
-              Total Payment
-            </Typography>
+          </Box>
+
+          <Typography className={styles["night_text"]}>
+            Extra Features
+          </Typography>
+          <Box>
+            {extraFeatures.map((extraFeature) => (
+              <Grid
+                key={extraFeature.feature}
+                container
+                className={styles["card_header"]}
+                sx={{ marginBottom: "0" }}
+              >
+                <Grid item xs={2} md={2}>
+                  <Checkbox
+                    name={extraFeature.price.toString()}
+                    onChange={ExtraFeaturesChangeHandler}
+                  ></Checkbox>
+                </Grid>
+                <Grid item xs={8} md={8}>
+                  {extraFeature.feature}
+                </Grid>
+                <Grid className={styles["night_text"]} item xs={2} md={2}>
+                  ${extraFeature.price}
+                </Grid>
+              </Grid>
+            ))}
+          </Box>
+          <Grid container>
+            <Grid item xs={9}>
+              <Typography className={styles["night_text"]}>
+                Total Payment
+              </Typography>
+            </Grid>
+            <Grid item xs={3}>
+              <Typography variant="h6">${totalAmount}</Typography>
+            </Grid>
+            <Grid item xs={12}>
+              <Button
+                sx={{ width: "100%", margin: "5% auto" }}
+                type="submit"
+                variant="contained"
+                size="large"
+                color="primary"
+                className={styles["booknow_btn"]}
+              >
+                Book Now
+              </Button>
+            </Grid>
           </Grid>
-          <Grid item xs={3}>
-            <Typography variant="h6">${totalAmount}</Typography>
-          </Grid>
-          <Grid item xs={12}>
-            <Button
-              sx={{ width: "100%", margin: "5% auto" }}
-              type="submit"
-              variant="contained"
-              size="large"
-              color="primary"
-              className={styles["booknow_btn"]}
-            >
-              Book Now
-            </Button>
-          </Grid>
-        </Grid>
+        </form>
         <Typography className={styles["cardfooter"]}>
           You will not get charged yet
         </Typography>
