0d96983479d50fe9a1141b844c46e11eedaa2598
mokshada malpani <malpanimokshada@gmail.com>
2022-09-06 18:09:20 +0530

    started

--- src/components/ViewReview/ViewReview.js
+++ src/components/ViewReview/ViewReview.js
@@ -1,15 +1,29 @@
 import React from "react";
 
-import  Card  from "@mui/material/Card";
-import  Box  from "@mui/material/Box";
-
+import Card from "@mui/material/Card";
+import Box from "@mui/material/Box";
+import Grid from "@mui/material/Grid";
 
 const ViewReview = () => {
-  return(
-    <Card style={{padding:"20px",width:"80%",margin:"10%"}}>
-        Hi
+  return (
+    <Card variant="outlined" style={styles.mainCard}>
+      <Grid container display="flex" spacing={2}>
+        <Grid item xs={12} md={6}>
+          <A
+        </Grid>
+        <Grid item xs={12} md={6}>
+          GRid2
+        </Grid>
+      </Grid>
     </Card>
   );
 };
 
+const styles = {
+  mainCard: {
+    padding: "20px",
+    margin: "10%",
+    borderRadius: "10px",
+  },
+};
 export default ViewReview;
